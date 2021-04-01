b4cf2b6fe1cf08f72815b50028a18f5e57ffb815
koba <koba1mobile@gmail.com>
2021-04-02 02:28:23 +0900

    Add clickListener and set whole todoModel list

--- app/src/main/java/com/koba/kobatodo/presenter/TodoAdapter.kt
+++ app/src/main/java/com/koba/kobatodo/presenter/TodoAdapter.kt
@@ -1,15 +1,28 @@
 package com.koba.kobatodo.presenter
 
+import android.database.Observable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.koba.kobatodo.databinding.ItemTodoListBinding
 import com.koba.kobatodo.domain.TodoModel
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
 
 class TodoAdapter : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
     private val _todoList = arrayListOf<TodoModel>()
-    val todoList: List<TodoModel>
+    var todoList: List<TodoModel>
         get() = _todoList
+        set(value) {
+            _todoList.clear()
+            _todoList.addAll(value)
+            notifyDataSetChanged()
+        }
+
+    private val _clickSubject = PublishSubject.create<Pair<View, TodoModel>>()
+    val clickSubject: Subject<Pair<View, TodoModel>>
+        get() = _clickSubject
 
     override fun getItemCount() = _todoList.size
 
@@ -24,9 +37,18 @@ class TodoAdapter : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(_todoList[position])
     }
 
     inner class TodoViewHolder(binding: ItemTodoListBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener {
+                _clickSubject.onNext(it to _todoList[adapterPosition])
+            }
+        }
+
+        fun bind(item: TodoModel){
+
+        }
     }
 }
